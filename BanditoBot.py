08261e938cff0dab185c11542d5fcf6d92056935
Josh R <jriver35@uncc.edu>
2020-08-25 17:01:58 -0400

    added memer channel moderation

--- BanditoBot.py
+++ BanditoBot.py
@@ -11,4 +11,16 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 client = discord.Client()
 
+@client.event
+async def on_message(message):
+    message.content.lower()
+    
+    # Make sure bot is not responding to itself
+    if message.author == client.user:
+        return
+        
+    # check if a message was sent to memers channel
+    if str(message.channel) == "😂-memers" and str(message.content) != "":
+        await message.channel.purge(limit=1)
+
 client.run(BOT_TOKEN)
