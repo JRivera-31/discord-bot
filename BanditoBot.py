9a90e41bc81abcc5cee2d44954cf53efef91cb48
Josh R <jriver35@uncc.edu>
2020-08-25 19:04:58 -0400

    added on ready function and commands set up

--- BanditoBot.py
+++ BanditoBot.py
@@ -1,26 +1,38 @@
 import discord
 import os
+
 from os.path import join, dirname
 from pathlib import Path
 from dotenv import load_dotenv
+from discord.ext import commands
 
+# Grab bot token from .env file
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-client = discord.Client()
+client = commands.Bot(command_prefix="!")
 
+# for events
 @client.event
+async def on_ready():
+    print("Bot is ready")
+
 async def on_message(message):
-    message.content.lower()
+    message.content = message.content.lower()
     
     # Make sure bot is not responding to itself
     if message.author == client.user:
         return
         
-    # check if a message was sent to memers channel
+    # check if a message with text was sent to memers channel
     if str(message.channel) == "😂-memers" and str(message.content) != "":
+        # delete the message
         await message.channel.purge(limit=1)
+
+# for commands
+@client.command()
+async def test(ctx):
+    await ctx.send("This is a test")
 
 client.run(BOT_TOKEN)
